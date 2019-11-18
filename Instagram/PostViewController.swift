134c6cb1db8fcab63e82ff3fe97827aa65ac6b06
yamaguchi_shota <yamaguchi_shota@kakaku.com>
2019-11-18 10:55:15 +0900

    Lesson8_4.1

---
+++ Instagram/PostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by 山口 彰太 on 2019/11/18.
+//  Copyright © 2019 shouta.yamaguchi4. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
